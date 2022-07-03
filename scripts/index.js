776f4182167821e46369ef38b42c658a3ef9b23c
Sergey Semerikov <sem.tool@yandex.ru>
2022-07-03 19:45:01 +0300

    правил ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -2,43 +2,47 @@ let page = document.querySelector(".page");
 let formElement = page.querySelector ('.popup');
 let addName =page.querySelector('.profile__edit');
 let remCross = page.querySelector('.popup__button');
+let nameInput = page.querySelector('.popup__input_person_name');
+let jobInput = page.querySelector('.popup__input_person_job');
+let textName = page.querySelector('.profile__title');
+let textJob = page.querySelector('.profile__subtitle');
 
-addName.addEventListener('click', function popChange(){
+
+function popChange(){
   formElement.classList.add("popup_opened");
-  });
-
-remCross.addEventListener('click', function opChange(){
-  formElement.classList.remove("popup_opened");
-  })
-
-let nameInput = page.querySelector('.popup__input_name');
-let jobInputt = page.querySelector('.popup__input_job');
-
-
-function formSubmitHandler (evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                              // Так мы можем определить свою логику отправки.
-                                              // О том, как это делать, расскажем позже.
- nameInput.value;
- jobInputt.value;
-                                   // Получите значение полей jobInput и nameInput из свойства value
- let textName = page.querySelector('.profile__title');
- let textJob = page.querySelector('.profile__subtitle');     // Выберите элементы, куда должны быть вставлены значения полей
- textName.textContent = nameInput.value;
- textJob.textContent = jobInputt.value;                       // Вставьте новые значения с помощью textContent
+  nameInput.value = textName.textContent;
+  jobInput.value = textJob.textContent;
 }
 
 
-formElement.addEventListener('click', formSubmitHandler);
+function notChange(){
+  formElement.classList.remove("popup_opened"); 
+}
+
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  textName.textContent = nameInput.value;
+  textJob.textContent = jobInput.value; 
+  notChange();
+}
+
+addName.addEventListener('click', popChange);
+remCross.addEventListener('click', notChange);
 formElement.addEventListener('submit', formSubmitHandler);
 
-let sub = page.querySelector('.popup__edit');
+
+
+/*
+textName.textContent = nameInput.value;
+textJob.textContent = jobInput.value;
+
+
+ let sub = page.querySelector('.popup__edit');
 
 sub.addEventListener('click', function popChen(){
   formElement.classList.remove("popup_opened");
   })
-
-  
 
 
 
@@ -53,7 +57,7 @@ sub.addEventListener('click', function popChen(){
 
 
 
-/* Находим форму в DOM
+Находим форму в DOM
 let formElement = // Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
 let nameInput = // Воспользуйтесь инструментом .querySelector()
